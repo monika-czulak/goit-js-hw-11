b654f82c0fb27b58cef7f1d4335daedff136930d
monika-czulak <czulak.monika@gmail.com>
2023-01-25 20:45:52 +0100

    update

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import { Notify } from 'notiflix';
+import axios from 'axios';
+import simpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const parameters = {
+  API_KEY: '33117991-e676598b9a80b077edaeb8d01',
+  IMG_TYPE: 'photo',
+  ORIENTATION: 'horizontal',
+  SAFE_SEARCH: 'true',
+  PER_PAGE: 40,
+};
+
